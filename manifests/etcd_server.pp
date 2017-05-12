c85de10497758e54f0282dc1b4f7041aaebdf15f
Samual Laakso <slaakso@hearst.com>
2017-05-12 13:35:12 -0400

    initial commit

---
+++ manifests/etcd_server.pp
@@ -0,0 +1,52 @@
+# install Kubernetes etcd backend
+class profiles::etcd_server(
+$dns_suffix   = hiera(dns_suffix)
+$etcd_version = hiera(etcd_version),
+$container_registry = hiera(container_registry),
+$etcd_peers_container = hiera(etcd_peers_container),
+) {
+  coreos::unit {'etcd3':
+    description  => 'etcd3',
+    after        => 'etcd-peers.service',
+    requires     => 'etcd-peers.service',
+    execstartpre => "/usr/bin/docker pull quay.io/coreos/etcd:${etcd_version}",
+    execstart    => "/usr/bin/docker run --name etcd3 \
+                    -v /var/lib/etcd3:/var/lib/etcd3 \
+                    -p 2379:2379 -p 2380:2380 \
+                    --env-file /etc/sysconfig/etcd-peers \
+                    --env-file /etc/sysconfig/etcd-vars \
+                    quay.io/coreos/etcd:${etcd_version}",
+    restartsec   => '5',
+    restart      => 'always',
+  }
+  coreos::unit {'etcd-peers':
+    description  => 'etcd debugging service',
+    execstartpre => '/usr/bin/curl -sSL -o /opt/bin/jq http://stedolan.github.io/jq/download/linux64/jq && /usr/bin/chmod +x /opt/bin/jq',
+    execstart    => '/usr/bin/bash -c "while true; do curl -sL http://127.0.0.1:4001/v2/stats/self | /opt/bin/jq . ; sleep 1 ; done"',
+  }
+  coreos::unit {'debug-etcd':
+    description  => 'track etcd peer nodes',
+    after        => 'docker.service',
+    requires     => 'docker.service',
+    execstartpre => "/usr/bin/docker pull ${container_registry}/${etcd_peers_container}",
+    execstart    => "/usr/bin/docker run --rm=true -v /etc/sysconfig/:/etc/sysconfig/ ${container_registry}/${etcd_peers_container}",
+    restartsec   => '10',
+    restart      => 'on-failure',
+  }
+
+  file {'/etc/environment':
+    ensure  => file,
+    owner   => 0,
+    group   => 0,
+    mode    => '0644',
+    content => template('profiles/etc-environment.erb'),
+  }
+  file {'/etc/sysconfig/etcd-vars':
+    ensure  => file,
+    owner   => 0,
+    group   => 0,
+    mode    => '0644',
+    content => template('profiles/etcd-vars.erb'),
+  }
+
+}
---
+++ manifests/puppet.pp
@@ -0,0 +1,31 @@
+# Manage Puppet agent
+class profiles::puppet (
+$dns_suffix = hiera(dns_suffix)
+){
+  coreos::unit {'puppet-agent':
+    description  => 'puppet agent service',
+    after        => 'docker.service',
+    requires     => 'docker.service',
+    execstartpre => "/bin/bash -c '/usr/bin/docker inspect %n &> /dev/null && /usr/bin/docker rm %n || :'",
+    execstart    =>
+        "/usr/bin/docker run \
+        --name %n \
+        --net=host \
+        --privileged \
+        -v /media/staging:/opt/staging \
+        -v /var/lib/puppet:/var/lib/puppet \
+        -v /var/run/puppetlabs:/var/run/puppetlabs \
+        -v /home/core:/home/core \
+        -v /etc:/etc \
+        -v /run:/run:ro \
+        -v /usr/bin/systemctl:/usr/bin/systemctl:ro \
+        -v /usr/lib/systemd:/usr/lib/systemd:ro \
+        -v /usr/lib64/systemd:/usr/lib64/systemd:ro \
+        -v /lib64:/lib64:ro \
+        -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+        loxo33/puppet-in-docker \
+        agent --no-daemonize --logdest=console --server=puppetmaster.${dns_suffix} --environment=production",
+    restartsec   => '5s',
+    restart      => 'always',
+  }
+}
